888e455227ff06bda56043d2ffc96cf664143e99
Yousif A. Dhahir <Yusuf.it.01@gmail.com>
2022-10-17 20:04:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(arr, driverName) {
+    const matchingNames = arr.filter(
+      (name) => name.toLowerCase() == driverName.toLowerCase()
+    );
+    return matchingNames;
+  }
+  
+  function fuzzyMatch(arr, driverName) {
+    let str = driverName.charAt(0);
+    const firstLetterMatched = arr.filter((name) => str == name.charAt(0));
+    return firstLetterMatched;
+  }
+  
+  function matchName(obj, string) {
+    const matched = obj.filter((el) => el.name == string);
+    return matched;
+  }  
